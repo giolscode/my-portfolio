87e907c0b57daed501fa9093d3e694ec8e88314e
LOSAT Giovanni <giolstpro@gmail.com>
2025-11-06 21:27:09 +0100

    Style: apparence du bouton

--- components/CTAButton.jsx
+++ components/CTAButton.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const CTAButton = ({ label = "Contacter", onClick }) => {
+const CTAButton = ({ label = "Contacter", onClick, className = "" }) => {
   return (
     <button
       type="button"
       onClick={onClick}
+      className={`m-2 px-6 py-2 border-2 border-blue-600 text-blue-600 hover:bg-blue-600 hover:text-white font-semibold rounded-md transition bg-transparent ${className}`}
     >
       {label}
     </button>
