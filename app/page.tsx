c079e14125da748b0e208a43cb3de30a92a921f9
LOSAT Giovanni <giolstpro@gmail.com>
2025-11-09 21:51:35 +0100

    style: changement dispositon des composants

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,70 @@
 "use client";
-import { LightRays } from "@/components/ui/light-rays";
-import CTAButton from "@/components/CTAButton";
+
 import { useRouter } from "next/navigation";
+import { RetroGrid } from "@/components/ui/retro-grid";
+import { TypingAnimation } from "@/components/ui/typing-animation";
+
+
+import CTAButton from "@/components/CTAButton";
 import Image from "next/image";
+
 
 export default function Home() {
   const link = document.createElement("a");
   const router = useRouter();
 
   return (
-    <div className="flex flex-col">
-      <div className="min-h-screen flex flex-col items-center justify-center bg-black p-12">
-        <div className="text-center">
-          <LightRays />
-          <h2 className="text-3xl font-bold mb-4 text-white"> Giovanni LOSAT </h2>
-          <span className="text-xl text-gray-300"> Développeur full-stack à en devenir... </span>
-        </div>
+    <div className="relative flex flex-col min-h-screen">
+      <RetroGrid 
+        opacity={0.1}
+        angle={50}
+        cellSize={50}
+      />
 
-        <div>
+      <div className="min-h-screen h-screen w-full bg-black flex items-center justify-center ">
+        <img
+          src="/moi.jpeg"
+          className="rounded-full object-cover"
+          height={300}
+          width={350}
+        />
 
-          <CTAButton
-            label="Me contacter"
-            onClick={() => router.push('/contact')}
-          />
+        <div className="justify-center ml-20 text-white">
+          <h2 className="text-4xl font-bold">
+            Salut moi c'est Giovanni LOSAT - Développeur informatique . 👋🏽
+          </h2>
 
-          <CTAButton
-            label="Télécharger mon CV"
-            onClick={() => {
-              link.href = "/CV_LOSAT_Giovanni.pdf";
-              link.download = "CV_LOSAT.pdf";
-              link.click();
-            }}
-          />
+          <br />
+
+          <TypingAnimation className="text-lg font-bold">
+            je suis un étudiant en 3eme année de BUT informatique qui à été toujours passionée par ce domaine depuis mon plus jeune age.
+          </TypingAnimation>
+
+          <br />
+          <br />
+
+          <div className="flex justify-center mt-3">
+            <CTAButton
+              label="Me contacter"
+              onClick={() => router.push("/contact")}
+            />
+
+            <CTAButton
+              label="Télécharger mon CV"
+              onClick={() => {
+                link.href = "/CV_LOSAT_Giovanni.pdf";
+                link.download = "CV_LOSAT.pdf";
+                link.click();
+              }}
+            />
+          </div>
         </div>
       </div>
+
+      <div className="min-h-screen h-screen w-full bg-white flex items-center justify-center">
+        
+      </div>
+
     </div>
   );
 }
