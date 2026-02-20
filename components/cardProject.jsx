78a3b00562ca8c1da8aa177e5aec4537923d67b9
LOSAT Giovanni <giolstpro@gmail.com>
2026-02-20 14:33:58 +0100

    feat: changement css + ajout logo tech

--- components/cardProject.jsx
+++ components/cardProject.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import CTAButton from "./CTAButton";
 import { div } from "motion/react-client";
 
-const cardProject = ({ title, imageSrc, description, linkUrl }) => {
+const cardProject = ({ title, imageSrc, description, linkUrl,technos }) => {
   const handleClick = () => {
     const link = document.createElement("a");
     link.href = linkUrl;
@@ -10,21 +10,29 @@ const cardProject = ({ title, imageSrc, description, linkUrl }) => {
   };
 
   return (
-  <div className="m-4 p-[2px] rounded-lg bg-gradient-to-r from-yellow-400 to-orange-500">
-    <div className="flex flex-col bg-black space-y-6 p-8 text-white rounded-lg max-w-md mx-auto">
+    <div className="flex flex-col bg-transparent border-2 border-white space-y-6 p-8 text-white rounded-lg max-w-md mx-auto hover:border-yellow-500 transition-colors duration-300 h-full">
       <div className="relative group">
         <img
           src={imageSrc}
           alt={`Aperçu du projet ${title}`}
-          height={300}
-          width={300}
+          height={200}
+          width={400}
+          className="w-full h-50 object-cover"
         />
       </div>
       <h2 className="text-xl font-bold text-white mb-2">{title}</h2>
-      <p className="text-white max-w-lg"> {description} </p>
-      <CTAButton onClick={handleClick} label="Voir le projet" />
+      <div className="">
+        <img
+          src={`https://skillicons.dev/icons?i=${technos}`}
+          className="h-10"
+        />
+      </div>
+      <p className="text-gray-300 line-clamp-3 flex-grow"> {description} </p>
+      <div className=" mt-auto pt-2">
+        <CTAButton onClick={handleClick} label="Voir le projet" />
+      </div>
+      
     </div>
-  </div>
   );
 };
 
